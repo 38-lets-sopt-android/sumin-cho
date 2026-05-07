0e3c20c1fb3e622f7acd2a155da3b3c697763fcf
chosumin432 <01074260518k@gmail.com>
2026-05-08 05:10:23 +0900

    [feat] 개별 페이지 화면 구현

---
+++ app/src/main/java/com/example/letssopt/presentation/save/SaveScreen.kt
@@ -0,0 +1,35 @@
+package com.example.letssopt.presentation.save
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SaveScreen() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "보관함",
+            color = Color.White
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SaveScreenPreview() {
+    SaveScreen()
+}
